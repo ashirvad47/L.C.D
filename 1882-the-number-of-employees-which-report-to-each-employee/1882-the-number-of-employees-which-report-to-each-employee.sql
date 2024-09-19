0d053f3b90bf693da7b1a587e74b0d314fa8cd2d
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-09-19 09:53:47 +0530

    Time: 587 ms (70.97%), Space: 0B (100%) - LeetHub

--- 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,20 +1,3 @@
--- SELECT 
---   reports_to as employee_id, 
---   (SELECT name FROM Employees WHERE employee_id = e.reports_to) as name, 
---   Count(e.employee_id) as reports_count, 
---   Round(Avg(e.age),0) as average_age
--- FROM 
---   Employees e
--- WHERE 
---   e.reports_to IS NOT NULL
--- GROUP BY 
---   e.reports_to
--- HAVING 
---   Count(e.employee_id) > 0
--- ORDER BY 
---   reports_to;
-
-
 Select mgr.employee_id ,mgr.name, Count(emp.employee_id) as reports_count,Round(Avg(emp.age),0) as average_age
 From Employees emp
 Join Employees mgr
