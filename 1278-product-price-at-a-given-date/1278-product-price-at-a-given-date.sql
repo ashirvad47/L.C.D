39c7045b0bfa5d9c71645584de47ce56ce837f40
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-09-19 23:04:04 +0530

    Time: 665 ms (20.08%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,27 +1,28 @@
 # Write your MySQL query statement below
--- SELECT p.product_id, 
---      COALESCE(
---             (SELECT new_price 
---             FROM Products
---             WHERE product_id = p.product_id 
---                   AND change_date <= '2019-08-16' 
---             ORDER BY change_date DESC LIMIT 1), 10) AS price
--- FROM (SELECT DISTINCT product_id FROM Products) p
+-- method 1
+SELECT p.product_id, 
+     COALESCE(
+            (SELECT new_price 
+            FROM Products
+            WHERE product_id = p.product_id 
+                  AND change_date <= '2019-08-16' 
+            ORDER BY change_date DESC LIMIT 1), 10) AS price
+FROM (SELECT DISTINCT product_id FROM Products) p
 
-
-SELECT DISTINCT product_id, 10 AS price
-FROM Products
-WHERE product_id NOT IN (
-  SELECT DISTINCT product_id
-  FROM Products
-  WHERE change_date <= '2019-08-16'
-)
-UNION
-SELECT product_id, new_price AS price
-FROM Products
-WHERE (product_id, change_date) IN (
-  SELECT product_id, MAX(change_date) AS date
-  FROM Products
-  WHERE change_date <= '2019-08-16'
-  GROUP BY product_id
-)
+-- method 2
+-- SELECT DISTINCT product_id, 10 AS price
+-- FROM Products
+-- WHERE product_id NOT IN (
+--   SELECT DISTINCT product_id
+--   FROM Products
+--   WHERE change_date <= '2019-08-16'
+-- )
+-- UNION
+-- SELECT product_id, new_price AS price
+-- FROM Products
+-- WHERE (product_id, change_date) IN (
+--   SELECT product_id, MAX(change_date) AS date
+--   FROM Products
+--   WHERE change_date <= '2019-08-16'
+--   GROUP BY product_id
+-- )
