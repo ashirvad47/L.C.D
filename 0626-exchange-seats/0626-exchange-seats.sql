1429db250b6420e11a27c6d0f30d59687fe3203b
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-09-21 22:50:29 +0530

    Time: 318 ms (68.51%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -22,11 +22,8 @@
 -- ORDER BY id;
 
 
+
 # Write your MySQL query statement below
-SELECT ROW_NUMBER() OVER() id, student
+SELECT ROW_NUMBER() OVER() as id, student
 FROM seat
 ORDER BY IF(MOD(id, 2) = 0, id-1, id+1)
-
-
-
-
