f4f947ede8ea062e93e1c7071436e7c0f72eefa8
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-09-21 21:03:53 +0530

    Time: 323 ms (63.82%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -14,11 +14,19 @@
 
 
 # Write your MySQL query statement below
-SELECT IF (id < (SELECT MAX(id) FROM Seat), 
-            IF(id % 2 = 0, id - 1, id + 1), 
-            IF(id % 2 = 0, id - 1, id)
-        ) AS id, student
-FROM Seat
-ORDER BY id;
+-- SELECT IF (id < (SELECT MAX(id) FROM Seat), 
+--             IF(id % 2 = 0, id - 1, id + 1), 
+--             IF(id % 2 = 0, id - 1, id)
+--         ) AS id, student
+-- FROM Seat
+-- ORDER BY id;
+
+
+# Write your MySQL query statement below
+SELECT ROW_NUMBER() OVER() id, student
+FROM seat
+ORDER BY IF(MOD(id, 2) = 0, id-1, id+1)
+
+
 
 
