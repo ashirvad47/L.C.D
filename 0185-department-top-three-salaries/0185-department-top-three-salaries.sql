0b79ad29664cccac2c7bc14da68ac0f107cade45
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-09-23 12:54:55 +0530

    Time: 781 ms (85.36%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -7,3 +7,16 @@ From (
 Join Department d
 On e.departmentId = d.id
 Where e.denseRank <=3
+
+
+-- go through the belowclever solution while revising 
+
+-- select d.Name Department, e1.Name Employee, e1.Salary
+-- from Employee e1 
+-- join Department d
+-- on e1.DepartmentId = d.Id
+-- where 3 > (select count(distinct(e2.Salary)) 
+--                   from Employee e2 
+--                   where e2.Salary > e1.Salary 
+--                   and e1.DepartmentId = e2.DepartmentId
+--                   );
