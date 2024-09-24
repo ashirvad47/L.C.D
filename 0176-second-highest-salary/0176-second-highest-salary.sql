925084704ba2fd3758d4000af89c699f03734b30
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-09-24 09:13:33 +0530

    Time: 220 ms (87.63%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,21 @@
 # Write your MySQL query statement below
-SELECT max(Salary) AS SecondHighestSalary
+
+-- METHOD 1
+SELECT if(Count(Distinct salary)=1, null ,(SELECT Distinct salary FROM Employee
+ORDER BY salary DESC
+LIMIT 1 OFFSET 1)) AS SecondHighestSalary
 FROM Employee
-WHERE Salary < (SELECT max(Salary) FROM Employee)
+
+-- -- METHOD 2
+-- select
+-- (select distinct Salary 
+-- from Employee order by salary desc 
+-- limit 1 offset 1) 
+-- as SecondHighestSalary;
+-- -- When a subquery doesn't produce any result, its return value is NULL. This behavior is consistent with SQL standards.
+
+
+-- -- METHOD3(better one)
+-- SELECT max(Salary) AS SecondHighestSalary
+-- FROM Employee
+-- WHERE Salary < (SELECT max(Salary) FROM Employee)
