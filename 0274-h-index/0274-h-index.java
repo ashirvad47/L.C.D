e7161ff3fcf6670e71c08157103e370c7e5115a9
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-09-27 23:16:00 +0530

    Time: 2 ms (72.72%), Space: 41.7 MB (30.35%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,17 +1,15 @@
+import java.util.Arrays;
+
 class Solution {
-    public int hIndex(int[] arr) {
-        int n = arr.length;
-        for (int h = 1; h <= n; h++) {
-            int count = 0;
-            for (int i = 0; i < n; i++) {
-                if (arr[i] >= h) {
-                    count++;
-                }
-            }
-            if (count < h) {
-                return h - 1;
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n - i) {
+                return n - i;
             }
         }
-        return n;
+        return 0;
     }
 }
