e54a53bf0d41b9ba78ef805396afa4a13aeba5fa
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-05 15:06:57 +0530

    Time: 6 ms (16.6%), Space: 46.6 MB (93.84%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,16 +1,18 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-
-        int result [] = new int[2];
-        for(int i=0; i < numbers.length;i++){
-            for (int j=i+1;j < numbers.length;j++ ){
-                if(numbers[i]+numbers[j]==target){
-                    result[0]=i+1;
-                    result[1]=j+1;
-                    return result;
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = numbers.length;
+        int result[]= new int[2];
+        for(int i =0; i <n;i++){
+            int complement = target- numbers[i];
+            if (map.containsKey(complement)) {
+                result[0]= map.get(complement) +1;  // Index of the complement
+                result[1]= i +1;  // This would be put in the second index as it'd be possible to find a complement only when the complement is stored previously and hence it'd obviously take the previous index.
+                return result;
             }
-        }
-        return result;
+            else{
+                map.put(numbers[i],i);
+            }
+        }return result;
     }
 }
