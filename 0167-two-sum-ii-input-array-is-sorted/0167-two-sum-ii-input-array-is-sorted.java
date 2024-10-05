d4ed5ec1090353b70d2cce63543ebd00a8cba096
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-05 15:15:12 +0530

    Time: 2 ms (92.09%), Space: 47.3 MB (28.85%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,18 +1,36 @@
+// class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int n = numbers.length;
+//         int result[]= new int[2];
+//         for(int i =0; i <n;i++){
+//             int complement = target- numbers[i];
+//             if (map.containsKey(complement)) {
+//                 result[0]= map.get(complement) +1;  // Index of the complement
+//                 result[1]= i +1;  // This would be put in the second index as it'd be possible to find a complement only when the complement is stored previously and hence it'd obviously take the previous index.
+//                 return result;
+//             }
+//             else{
+//                 map.put(numbers[i],i);
+//             }
+//         }return result;
+//     }
+// }
+
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int n = numbers.length;
-        int result[]= new int[2];
-        for(int i =0; i <n;i++){
-            int complement = target- numbers[i];
-            if (map.containsKey(complement)) {
-                result[0]= map.get(complement) +1;  // Index of the complement
-                result[1]= i +1;  // This would be put in the second index as it'd be possible to find a complement only when the complement is stored previously and hence it'd obviously take the previous index.
-                return result;
+        int left=0;
+        int right=numbers.length-1;
+        
+        while(numbers[left]+numbers[right]!= target){
+            if(numbers[left]+numbers[right] < target){
+                left++; //since the array is in ascending order, left++ makes the sum bigger
             }
             else{
-                map.put(numbers[i],i);
+                right--; //since the array is in ascending order, right-- makes the sum lesser
             }
-        }return result;
+        }
+        int [] result= {left+1,right+1};
+        return result;
     }
 }
