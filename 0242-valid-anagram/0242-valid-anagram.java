ced0fe03d32f65fae5be3dd34d5ca6c4f0c9d047
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-09 14:57:36 +0530

    Time: 12 ms (33.15%), Space: 45 MB (8.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,3 @@
-// class Solution {
-//     public boolean isAnagram(String s, String t) {
-//         char chs[] = s.toCharArray();
-//         char cht[] = t.toCharArray();
-
-//         Arrays.sort(chs);
-//         Arrays.sort(cht);
-
-//        if( Arrays.compare(chs,cht)==0) {
-//         return true;
-//        }else{
-//         return false;
-//        }
-
-
-//     }
-// }
-
 class Solution {
     public boolean isAnagram(String s, String t) {
         HashMap<Character, Integer> map = new HashMap<>();
