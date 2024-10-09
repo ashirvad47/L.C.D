0b7411616b639db2476b67ee511a2c6241d7c086
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-09 13:34:19 +0530

    Time: 12 ms (51.45%), Space: 57.4 MB (68.79%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,26 +1,24 @@
-// class Solution {
-//     public boolean containsDuplicate(int[] nums) {
-//        boolean repeat= false;
-//         int start = 0;
-//         int end= nums.length-1;
-//        while(start<end){
-//         if(nums[start]==nums[end]){
-//             return true;
-//         }
-//         start++;
-//         end--;
-//        } 
-//        return repeat;
-//     }
-// }
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-       for(int i =0; i < nums.length-1;i++){
-        if(nums[i]==nums[i+1]){
-            return true;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : nums){
+            if(set.contains(i)) {
+                return true;
+            } else {
+                set.add(i);
+            }
         }
-       }
-       return false;
+        return false;
     }
 }
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+//        for(int i =0; i < nums.length-1;i++){
+//         if(nums[i]==nums[i+1]){
+//             return true;
+//         }
+//        }
+//        return false;
+//     }
+// }
