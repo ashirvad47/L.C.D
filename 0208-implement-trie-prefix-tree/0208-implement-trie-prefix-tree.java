d4e028b490551317b8958ac3602615065f611bd6
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-12 20:10:29 +0530

    Time: 36 ms (62.03%), Space: 55.4 MB (33.43%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -0,0 +1,57 @@
+class Node {
+    Node[] children = new Node[26];
+    boolean eow;
+    Node() {
+        for (int i = 0; i < 26; i++) {
+            children[i] = null;
+        }
+        eow = false;
+    }
+}
+
+class Trie {
+    Node root; 
+
+    public Trie() {
+        root = new Node();
+    }
+
+    public void insert(String word) {
+        Node current = root; 
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (current.children[idx] == null) {
+                current.children[idx] = new Node();
+            }
+            current = current.children[idx];
+        }
+        current.eow = true; 
+    }
+
+    public boolean search(String word) {
+        Node current = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (current.children[idx] == null) {
+                return false; 
+            }
+            if (i == word.length() - 1 && current.children[idx].eow == false) {
+                return false;
+            }
+            current = current.children[idx];
+        }
+        return true;
+    }
+
+    public boolean startsWith(String prefix) {
+        Node current = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            int idx = prefix.charAt(i) - 'a';
+            if (current.children[idx] == null) {
+                return false;
+            }
+            current = current.children[idx];
+        }
+        return true; // Prefix found!
+    }
+}
