5e18abcd4fabad9e6bdaf1ce56da74a07b62d286
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-15 08:53:35 +0530

    Time: 14 ms (19.2%), Space: 44.9 MB (20.87%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,35 +1,55 @@
+// class Solution {
+//     public boolean isValidSudoku(char[][] board) {
+//         for (int i = 0; i < 9; i++) {
+//             HashSet<Character> row = new HashSet<>();
+//             for (int j = 0; j < 9; j++) {
+//                 if (board[i][j]!= '.') {
+//                     if (!row.add(board[i][j])) {  
+//                         return false;
+//                     }
+//                 }
+//             }
+//         }
+//         for (int i = 0; i < 9; i++) {
+//             HashSet<Character> column = new HashSet<>();
+//             for (int j = 0; j < 9; j++) {
+//                 if (board[j][i] != '.') {
+//                     if (!column.add(board[j][i])) { 
+//                         return false;
+//                     }
+//                 }
+//             }
+//         }
+//         HashSet<Character>[] mini = new HashSet[9];
+//         for (int i = 0; i < 9; i++) {
+//             mini[i] = new HashSet<>();
+//         }
+
+//         for (int i = 0; i < 9; i++) {
+//             for (int j = 0; j < 9; j++) {
+//                 if (board[i][j] != '.') {
+//                     int miniIndex = (i / 3) * 3 + (j / 3);
+//                     if (!mini[miniIndex].add(board[i][j])) { 
+//                         return false;
+//                     }
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        for (int i = 0; i < 9; i++) {
-            HashSet<Character> row = new HashSet<>();
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j]!= '.') {
-                    if (!row.add(board[i][j])) {  
-                        return false;
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < 9; i++) {
-            HashSet<Character> column = new HashSet<>();
-            for (int j = 0; j < 9; j++) {
-                if (board[j][i] != '.') {
-                    if (!column.add(board[j][i])) { 
-                        return false;
-                    }
-                }
-            }
-        }
-        HashSet<Character>[] mini = new HashSet[9];
-        for (int i = 0; i < 9; i++) {
-            mini[i] = new HashSet<>();
-        }
+        Set<String> seen = new HashSet<>();
 
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {
-                    int miniIndex = (i / 3) * 3 + (j / 3);
-                    if (!mini[miniIndex].add(board[i][j])) { 
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                char number = board[i][j];
+                if (number != '.') {
+                    if (!seen.add(number + " in row " + i) ||
+                            !seen.add(number + " in column " + j) ||
+                            !seen.add(number + " in block " + i / 3 + "-" + j / 3)) {
                         return false;
                     }
                 }
