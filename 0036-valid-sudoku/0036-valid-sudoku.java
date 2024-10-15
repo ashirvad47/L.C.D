8de3cdce3a2387af826e50b1c0d2f705b85abec1
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-15 08:38:57 +0530

    Time: 2 ms (79.42%), Space: 44.1 MB (80.28%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,32 +1,25 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        // Check each row
         for (int i = 0; i < 9; i++) {
             HashSet<Character> row = new HashSet<>();
             for (int j = 0; j < 9; j++) {
-                char current = board[i][j];
-                if (current != '.') {
-                    if (!row.add(current)) {  // If add returns false, it means duplicate exists
+                if (board[i][j]!= '.') {
+                    if (!row.add(board[i][j])) {  
                         return false;
                     }
                 }
             }
         }
-
-        // Check each column
         for (int i = 0; i < 9; i++) {
             HashSet<Character> column = new HashSet<>();
             for (int j = 0; j < 9; j++) {
-                char current = board[j][i];
-                if (current != '.') {
-                    if (!column.add(current)) {  // If add returns false, duplicate exists
+                if (board[j][i] != '.') {
+                    if (!column.add(board[j][i])) { 
                         return false;
                     }
                 }
             }
         }
-
-        // Check each 3x3 sub-grid
         HashSet<Character>[] mini = new HashSet[9];
         for (int i = 0; i < 9; i++) {
             mini[i] = new HashSet<>();
@@ -34,17 +27,14 @@ class Solution {
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                char current = board[i][j];
-                if (current != '.') {
+                if (board[i][j] != '.') {
                     int miniIndex = (i / 3) * 3 + (j / 3);
-                    if (!mini[miniIndex].add(current)) {  // If add returns false, duplicate exists
+                    if (!mini[miniIndex].add(board[i][j])) { 
                         return false;
                     }
                 }
             }
         }
-
-        // If no duplicates are found, the board is valid
         return true;
     }
 }
