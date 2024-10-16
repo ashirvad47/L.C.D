578a558b0601cbaa42a105d025e1fb515addb899
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-16 20:40:00 +0530

    Time: 0 ms (100%), Space: 42 MB (67.75%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            int x = Arrays.binarySearch(matrix[i], target);
+            if (x >= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
