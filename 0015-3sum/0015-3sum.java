98a7f7b03112220d773cc88cb9e438d7d2712452
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-18 19:00:04 +0530

    Time: 230 ms (26.87%), Space: 51.9 MB (39.55%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,21 +1,38 @@
-class Solution {
+public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums); // Sorted Array
+        List<List<Integer>> answer = new ArrayList<>();
         
-        HashSet<List<Integer>> hs = new HashSet<>();
-        for(int i =0; i<nums.length-2;i++){
-            HashMap<Integer,Integer> map = new HashMap <>();
-            for(int j=i+1;j<nums.length;j++){
-                int complement= -nums[i]-nums[j];
-                if (map.containsKey(complement)) {
-                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
-                    Collections.sort(triplet);
-                    hs.add(triplet);
-                }
-                else{
-                    map.put(nums[j],j);
-                }
-            }
+        if (nums.length < 3) {
+            return answer;
         }
-        return new ArrayList<>(hs);
+        
+        if (nums[0] > 0) {
+            return answer;
+        }
+        
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; ++i) {
+            hashMap.put(nums[i], i);
+        }
+        
+        for (int i = 0; i < nums.length - 2; ++i) {
+            if (nums[i] > 0) {
+                break;
+            }
+            
+            for (int j = i + 1; j < nums.length - 1; ++j) {
+                int required = -1 * (nums[i] + nums[j]);
+                if (hashMap.containsKey(required) && hashMap.get(required) > j) {
+                    answer.add(Arrays.asList(nums[i], nums[j], required));
+                }
+                j = hashMap.get(nums[j]);
+            }
+            
+            i = hashMap.get(nums[i]);
+        }
+        
+        return answer;
     }
 }
