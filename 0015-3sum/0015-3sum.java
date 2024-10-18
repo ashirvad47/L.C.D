7ed32260999b65b7fd76fa2667c22bf839bff69f
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-18 21:39:43 +0530

    Time: 31 ms (58.12%), Space: 51.2 MB (85.91%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,7 +5,7 @@ class Solution {
         // Sort the array
         Arrays.sort(nums);
         
-        for (int i = 0; i < nums.length - 2; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
             // Skip duplicate elements
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
