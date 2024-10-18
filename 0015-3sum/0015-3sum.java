b9f764b3ec8b0c13c2ae5cf82f3ef1b0e0f3673b
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-18 21:17:52 +0530

    Time: 30 ms (67.86%), Space: 51.2 MB (85.91%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,25 +1,45 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        // Sort the array
         Arrays.sort(nums);
-        Set<List<Integer>> s = new HashSet<>();
-        List<List<Integer>> output = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            int j = i + 1;
-            int k = nums.length - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate elements
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
                 if (sum == 0) {
-                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // Skip duplicates for the second pointer
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    // Skip duplicates for the third pointer
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    
+                    // Move both pointers
+                    left++;
+                    right--;
                 } else if (sum < 0) {
-                    j++;
+                    left++; // We need a larger sum
                 } else {
-                    k--;
+                    right--; // We need a smaller sum
                 }
             }
         }
-        output.addAll(s);
-        return output;
+        
+        return result;
     }
 }
