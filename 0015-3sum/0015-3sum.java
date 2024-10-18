efcb535e2073960607be247888b2bbe448cdefec
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-18 19:11:42 +0530

    Time: 864 ms (10.55%), Space: 52.1 MB (31.45%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,38 +1,25 @@
-public class Solution {
+class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums); // Sorted Array
-        List<List<Integer>> answer = new ArrayList<>();
-        
-        if (nums.length < 3) {
-            return answer;
-        }
-        
-        if (nums[0] > 0) {
-            return answer;
-        }
-        
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; ++i) {
-            hashMap.put(nums[i], i);
-        }
-        
-        for (int i = 0; i < nums.length - 2; ++i) {
-            if (nums[i] > 0) {
-                break;
-            }
-            
-            for (int j = i + 1; j < nums.length - 1; ++j) {
-                int required = -1 * (nums[i] + nums[j]);
-                if (hashMap.containsKey(required) && hashMap.get(required) > j) {
-                    answer.add(Arrays.asList(nums[i], nums[j], required));
+        Arrays.sort(nums);
+        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
                 }
-                j = hashMap.get(nums[j]);
             }
-            
-            i = hashMap.get(nums[i]);
         }
-        
-        return answer;
+        output.addAll(s);
+        return output;
     }
 }
