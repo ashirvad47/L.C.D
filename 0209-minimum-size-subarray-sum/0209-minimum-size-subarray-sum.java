db815595f26d7216829f416db8296d560f40f957
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-22 15:57:55 +0530

    Time: 1 ms (99.57%), Space: 58.3 MB (21.13%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,22 +1,38 @@
+// class Solution {
+//     public int minSubArrayLen(int target, int[] nums) {
+
+//         int sum = 0;
+//         int len = Integer.MAX_VALUE;
+//         for (int i = 0, j = 0; i < nums.length; i++) {
+//             sum += nums[i];
+//             if (sum >= target) {
+//                 len = Math.min(len, i - j + 1);
+//                 j += 1;
+//                 i = j - 1;
+//                 sum = 0;
+//             }
+//         }
+//         return len < Integer.MAX_VALUE ? len : 0;
+//     }
+// }
+
+// better sol below
+
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        // HashMap<Integer,Integer> map = new HashMap<>();
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
 
-        // for(int i :nums){
-        //     map.put(i,map.getOrDefault(i,0)+1);
-        // }
+        for (int i = 0,j=0; i < nums.length; i++) {
+            sum += nums[i];
 
-        int sum=0;
-        int len=Integer.MAX_VALUE;
-        for(int i = 0,j=0; i<nums.length;i++){
-            sum+=nums[i];
-            if(sum>=target){
-                len = Math.min(len, i-j+1);
-                j +=1;
-                i=j-1;
-                sum=0;
+            while (sum >= target) {
+                minLength = Math.min(minLength, i - j + 1);
+                sum -= nums[j];
+                j++;
             }
         }
-        return len < Integer.MAX_VALUE ? len:0;
+
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
     }
 }
