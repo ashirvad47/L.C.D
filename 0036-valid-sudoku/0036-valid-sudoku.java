0a3e492e24a98bb728b169c3153fa4fe6f6e8ea3
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-24 19:53:28 +0530

    Time: 2 ms (79.55%), Space: 44.6 MB (42.13%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -3,8 +3,8 @@ class Solution {
         for (int i = 0; i < 9; i++) {
             HashSet<Character> row = new HashSet<>();
             for (int j = 0; j < 9; j++) {
-                if (board[i][j]!= '.') {
-                    if (!row.add(board[i][j])) {  
+                if (board[i][j] != '.') {
+                    if (!row.add(board[i][j])) {
                         return false;
                     }
                 }
@@ -14,7 +14,7 @@ class Solution {
             HashSet<Character> column = new HashSet<>();
             for (int j = 0; j < 9; j++) {
                 if (board[j][i] != '.') {
-                    if (!column.add(board[j][i])) { 
+                    if (!column.add(board[j][i])) {
                         return false;
                     }
                 }
@@ -28,8 +28,8 @@ class Solution {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] != '.') {
-                    int miniIndex = (i / 3) * 3 + (j / 3);
-                    if (!mini[miniIndex].add(board[i][j])) { 
+                    int miniIndex = (i / 3) * 3 + (j / 3);//imp part 
+                    if (!mini[miniIndex].add(board[i][j])) {
                         return false;
                     }
                 }
@@ -38,3 +38,23 @@ class Solution {
         return true;
     }
 }
+
+// class Solution {
+//     public boolean isValidSudoku(char[][] board) {
+//         Set<String> seen = new HashSet<>();
+
+//         for (int i = 0; i < 9; ++i) {
+//             for (int j = 0; j < 9; ++j) {
+//                 char number = board[i][j];
+//                 if (number != '.') {
+//                     if (!seen.add(number + " in row " + i) ||
+//                             !seen.add(number + " in column " + j) ||
+//                             !seen.add(number + " in block " + i / 3 + "-" + j / 3)) {
+//                         return false;
+//                     }
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
