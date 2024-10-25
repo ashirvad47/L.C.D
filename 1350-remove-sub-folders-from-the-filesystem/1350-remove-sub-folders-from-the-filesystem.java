f4ea39cf93eb98a27a0a7827a8ca90974145bd8d
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-25 09:32:00 +0530

    Time: 49 ms (42.65%), Space: 55.7 MB (46.94%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.java
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.java
@@ -1,55 +1,49 @@
-import java.util.*; 
-
-class Solution {	
-    Node root = new Node();         //Trie Root
-    List<String> ans = new ArrayList<>();
-    
+class Trie{
+    class Node{
+        boolean eow;
+        Node[]children;
+        Node(){
+            children=new Node[27];
+        }
+    }
+    Node root;
+    Trie(){
+        root=new Node();
+    }
+    public void insert(String s){
+        Node curr=root;
+        for(char ch:s.toCharArray()){
+            int idx=ch-'a';
+            if(ch=='/')idx=26;
+            if(curr.children[idx]==null)curr.children[idx]=new Node();
+            curr=curr.children[idx];
+        }
+        curr=curr.children[26]=new Node();
+        curr.eow=true;
+    }
+    public boolean search(String s){
+        Node curr=root;
+        for(char ch:s.toCharArray()){
+            int idx=ch-'a';
+            if(ch=='/')idx=26;
+            if(curr.children[idx]==null)return false;
+            curr=curr.children[idx];
+            if(curr.eow==true)return true;
+        }
+        return false;
+    }
+}
+class Solution {
     public List<String> removeSubfolders(String[] folder) {
-        for(String fl : folder) {
-            insert(fl);
+        Trie trie=new Trie();
+        Arrays.sort(folder);
+        List<String>ans=new ArrayList<>();
+        for(String s:folder){
+            if(!trie.search(s)){
+                ans.add(s);
+                trie.insert(s);
+            }
         }
-        
-        for(Node child : root.children.values()) {
-            findFirstNonNull(child);
-        }
-        
         return ans;
     }
-    
-    private void findFirstNonNull(Node node) {
-        if(node.word != null) {
-            ans.add(node.word);
-            return;
-        }
-        
-        for(Node child : node.children.values()) {
-            findFirstNonNull(child);
-        }
-    }
-    
-    private void insert(String folder) {
-        Node curr = root;        
-        StringTokenizer tokeniser = new StringTokenizer(folder, "/");             //more efficient than string.split() 
-		
-        while(tokeniser.hasMoreTokens()) {			
-			//stop insertion of folder into trie if root folder encountered
-            if(curr.word != null) {
-                return;
-            }
-						
-            String str = tokeniser.nextToken();
-            if(!curr.children.containsKey(str)) {
-                curr.children.put(str, new Node());
-            }			
-            curr = curr.children.get(str);
-        }
-        curr.word = folder;
-    }
-    
-}
-
-//Trie Node declaration - storing word in the end node for easy reference
-class Node{
-    Map<String, Node> children = new HashMap<>();
-    String word;
 }
