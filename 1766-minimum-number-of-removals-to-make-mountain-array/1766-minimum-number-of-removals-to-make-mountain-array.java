4ac83053f4338b185e9cd539e1ffb7caa248d01c
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-10-30 09:57:59 +0530

    Time: 34 ms (80%), Space: 44.6 MB (22.45%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.java
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.java
@@ -1,34 +1,41 @@
 class Solution {
     public int minimumMountainRemovals(int[] nums) {
-        
         int n = nums.length;
-        int[] LIS = new int[n];
-        int[] LDS = new int[n];
-    
-        Arrays.fill(LIS, 1);
-        Arrays.fill(LDS, 1);
+		//step:1 find the longest increasing subsequences till i
+        int[] lis = new int[n];
         
-        for(int i=1 ; i < n ; i++)
-        {
-            for(int j = 0 ; j < i ; j++)
-            {
-                if(nums[i] > nums[j] && LIS[j]+1 > LIS[i])
-                    LIS[i] = LIS[j]+1;
+        for (int i = 0; i < n; i++){
+            int max = 1;
+            for (int j = 0;j <i; j++){
+                if (nums[j] < nums[i]){
+                    max = Math.max(max, lis[j] + 1);
+                }
             }
+             lis[i] = max;
         }
         
-        int max = 0;
-        for(int i=n-2 ; i >= 0 ; i--)
-        {
-            for(int j = n-1 ; j > i ; j--)
-            {
-                if(nums[i] > nums[j] && LDS[j]+1 > LDS[i])
-                    LDS[i] = LDS[j]+1;
+		//step : 2 find longest decreasing subsequence starting from i
+        int[] lds = new int[n];
+        for (int i = n-1; i >= 0; i--){
+            int max = 1;
+            for (int j = i+1; j < n; j++){
+                if (nums[i] > nums[j]){
+                    max = Math.max(max, lds[j]+ 1);
+                }
             }
-            
-            if(LIS[i] > 1 && LDS[i] > 1) 
-                max = Math.max(LIS[i]+LDS[i]-1, max);
+            lds[i] = max;
         }
-        return n - max;
+        
+		/*step 3: now find longest bitonic subsequence 
+		but ensure that there something on the left and right of a particular index i 
+		inorder to make it a mountain  which means lis[i] > 1 and lds[i] > 1
+		*/
+        int lbs = 0;
+        for (int i = 0; i < n; i++){
+            if (lis[i] > 1 && lds[i] > 1)
+            lbs = Math.max(lis[i] + lds[i]-1, lbs);
+        }
+		//step 4: min remove is size of the original array - the length of the longest bitonic              subsequence found
+        return n - lbs;
     }
 }
