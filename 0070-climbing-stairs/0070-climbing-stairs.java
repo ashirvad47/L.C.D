79be80a3fb7952338b41a42b2a9ab3ce5d5da0c1
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-11-01 08:57:53 +0530

    Time: 0 ms (100%), Space: 40.5 MB (19.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,40 @@
+// //Dynamic Programming
+
+// class Solution {
+//     public int climbStairs(int n) {
+
+//         if (n == 1)
+//             return 1;
+//         if (n == 2)
+//             return 2;
+
+//         int dp[] = new int[n + 1];
+//         dp[1] = 1;
+//         dp[2] = 2;
+//         for (int i = 3; i <= n; i++) {
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
+//         return dp[n];
+//     }
+// }
+
+// Without DP
 class Solution {
     public int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
 
-        if(n==1) return 1;
-        if(n==2) return 2;
+        int prev1 = 2;
+        int prev2 = 1;
 
-        int dp[] = new int[n+1];
-        dp[1]=1;
-        dp[2]=2;
-        for(int i=3;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+        for (int i = 3; i <= n; i++) {
+            int current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
         }
-        return dp[n];
+
+        return prev1;
     }
 }
