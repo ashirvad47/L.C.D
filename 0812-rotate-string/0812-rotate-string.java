ae3ec407c9da4e723bda4d4c15e47a47715c22d3
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-11-03 18:15:50 +0530

    Time: 0 ms (100%), Space: 41.3 MB (66.51%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,13 +1,19 @@
+// class Solution {
+//     public boolean rotateString(String s, String goal) {
+//         if (s.length() != goal.length())
+//             return false;
+
+//         for (int i = 0; i < s.length(); i++) {
+//             String str = goal.substring(i) + goal.substring(0, i);
+//             if (s.equals(str))
+//                 return true;
+//         }
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if (s.length() != goal.length())
-            return false;
-
-        for (int i = 0; i < s.length(); i++) {
-            String str = goal.substring(i) + goal.substring(0, i);
-            if (s.equals(str))
-                return true;
-        }
-        return false;
+        return s.length() == goal.length() && (s + s).contains(goal);
     }
 }
