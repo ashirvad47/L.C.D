0e0edaa150fdca7ad087661c04b362ad4787fc03
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-11-16 09:51:18 +0530

    Time: 5 ms (98%), Space: 42.2 MB (84.96%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,27 +1,22 @@
-import java.util.HashMap;
-
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Character> mapS = new HashMap<>();
-        HashMap<Character, Character> mapT = new HashMap<>();
+        if (s.length() != t.length()) return false;
+
+        int[] mapS = new int[256]; // Maps characters in s to t
+        int[] mapT = new int[256]; // Maps characters in t to s
 
         for (int i = 0; i < s.length(); i++) {
-            char charS = s.charAt(i);
-            char charT = t.charAt(i);
+            char charS = s.charAt(i); // Current character from s
+            char charT = t.charAt(i); // Current character from t
 
-            if (mapS.containsKey(charS)) {
-                if (mapS.get(charS) != charT) return false; 
-            } else {
-                mapS.put(charS, charT);
+            if (mapS[charS] != mapT[charT]) {
+                return false; // Mappings don't match
             }
 
-            if (mapT.containsKey(charT)) {
-                if (mapT.get(charT) != charS) return false; 
-            } else {
-                mapT.put(charT, charS);
-            }
+            mapS[charS] = i + 1; // Update mapping for s -> t
+            mapT[charT] = i + 1; // Update mapping for t -> s
         }
 
-        return true; 
+        return true;
     }
 }
