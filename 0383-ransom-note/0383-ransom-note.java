d8216ebce45ec92bf8d7b480108c7ed70fa49225
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-11-16 08:20:59 +0530

    Time: 2 ms (83.93%), Space: 44.5 MB (87.2%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,15 +1,31 @@
+// class Solution {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+//         HashMap<Character,Integer> map = new HashMap<>();
+
+//         for(char i :  magazine.toCharArray()){
+//             map.put(i,map.getOrDefault(i,0)+1);
+//         }
+//         for(char i : ransomNote.toCharArray()){
+//             if(map.containsKey(i) && map.get(i)>0){
+//                 map.put(i,map.get(i)-1);
+//             }
+//             else return false;
+//         }
+//         return true;
+//     }
+// }
+//without hashmap
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> map = new HashMap<>();
+		if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
+        
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
 
-        for(char i :  magazine.toCharArray()){
-            map.put(i,map.getOrDefault(i,0)+1);
-        }
-        for(char i : ransomNote.toCharArray()){
-            if(map.containsKey(i) && map.get(i)>0){
-                map.put(i,map.get(i)-1);
-            }
-            else return false;
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
         }
         return true;
     }
