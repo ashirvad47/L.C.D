3d24ba96c2ab6a094619b2e1027f6ea2247984ef
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-11-28 11:10:55 +0530

    Time: 1 ms (81.38%), Space: 41.4 MB (50.4%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,12 +1,27 @@
 class Solution {
-    public boolean wordPattern(String pattern, String str) {
-    String[] words = str.split(" ");
-    if (words.length != pattern.length())
-        return false;
-    Map index = new HashMap();
-    for (Integer i=0; i<words.length; ++i)
-        if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+    
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> map = new HashMap<>();
+
+        String[] words = s.split(" ");
+
+        if (pattern.length() != words.length) {
             return false;
-    return true;
-}
+        }
+
+        for (int i = 0; i < words.length; i++) {
+            if (map.containsKey(pattern.charAt(i))) {
+                if (!map.get(pattern.charAt(i)).equals(words[i])) {
+                    return false;
+                }
+            } else {
+                if (map.containsValue(words[i])) {
+                    return false;
+                }
+                map.put(pattern.charAt(i), words[i]);
+            }
+        }
+
+        return true;
+    }
 }
