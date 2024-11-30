3e3bfb27c8f09f8aeeaae794529dac9c17621bd0
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-11-30 11:45:43 +0530

    Time: 0 ms (100%), Space: 40.5 MB (68.41%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,13 +1,17 @@
+// Floyd’s Tortoise and Hare algorithm
+
 class Solution {
     public boolean isHappy(int n) {
+        if(n==1) return true;
+        if(n==2 || n==3) return false;
       int slow=n;
       int fast=n;
         do{
             slow=sumSquare(slow);
             fast=sumSquare(sumSquare(fast));
-        }while(slow!=fast);
+        }while(slow!=fast && (slow!=1 || fast!=1));
         
-        if(slow==1){
+        if(slow==1 || fast==1){
             return true;
         }
         return false;
@@ -22,3 +26,31 @@ class Solution {
         return n;
     }
 }
+
+
+// class Solution {
+//     public boolean isHappy(int n) {
+//         if(n==1) return true;
+//         if(n==2 || n==3) return false;
+
+//         HashSet<Integer> set = new HashSet<>();
+//         int sum=n;
+//         while(sum!=1 && set.contains(sum)==false){
+           
+//            set.add(sum);
+//            sum= sumSquare(sum);
+//         }
+//        return sum==1;
+//     }
+
+//     public int sumSquare(int n){
+//         int sum =0;
+
+//         while(n >0){
+//             int dig = n%10;
+//             sum += dig*dig;
+//             n /= 10;
+//         }
+//         return sum;
+//     }
+// }
