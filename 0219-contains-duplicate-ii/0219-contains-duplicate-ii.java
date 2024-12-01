4120b9d394399fbd71fd46affd569d9ba3c92c85
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-12-01 11:14:54 +0530

    Time: 15 ms (98.07%), Space: 57.1 MB (66.7%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -19,8 +19,7 @@ class Solution {
         Set<Integer> set = new HashSet<Integer>();
         for(int i = 0; i < nums.length; i++){
             if(i > k) set.remove(nums[i-k-1]);
-            if(set.contains(nums[i])) return true;
-            set.add(nums[i]);
+            if(!set.add(nums[i])) return true;
         }
         return false;
  }
