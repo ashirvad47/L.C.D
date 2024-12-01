73d3b9c54639dfbf558b8fe7714af9aeb3d6ad05
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-12-01 16:15:38 +0530

    Time: 31 ms (55.81%), Space: 60.7 MB (71.19%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,32 +1,61 @@
+// Efficient
+
 class Solution {
     public int longestConsecutive(int[] nums) {
-  int max = 0;
-  
-  Set<Integer> set = new HashSet<Integer>();
-  for (int i = 0; i < nums.length; i++) {
-    set.add(nums[i]);
-  }
-  
-  for (int i = 0; i < nums.length; i++) {
-    int count = 1;
-    
-    // look left
-    int num = nums[i];
-    while (set.contains(--num)) {
-      count++;
-      set.remove(num);
+        int max = 0;
+
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int count = 1;
+            int num = nums[i];
+            // look left
+            
+            while (set.contains(num-1)) {
+                count++;
+                num-=1;
+                set.remove(num);
+            }
+
+            // look right
+            num = nums[i];
+            while (set.contains(num+1)) {
+                count++;
+                num+=1;
+                set.remove(num);
+            }
+
+            max = Math.max(max, count);
+        }
+
+        return max;
     }
-    
-    // look right
-    num = nums[i];
-    while (set.contains(++num)) {
-      count++;
-      set.remove(num);
-    }
-    
-    max = Math.max(max, count);
-  }
-  
-  return max;
 }
-}
+
+
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+//         for(int i :nums){
+//             set.add(i);
+//         }
+
+//         int maxCount=0;
+//         for(int i =0;i < nums.length;i++){
+//             int count =1;
+//             if(!set.contains(nums[i]-1)){
+//                 int x=nums[i];
+//                 while(set.contains(++x)){
+//                     count++;
+//                 }
+//             }
+//             maxCount = Math.max(count,maxCount);
+//         }
+
+//         return maxCount;
+        
+//     }
+// }
