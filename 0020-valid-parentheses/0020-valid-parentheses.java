5f15d9054ed372d2edf2507e7d2efba717363bb2
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-12-02 20:55:37 +0530

    Time: 2 ms (97.48%), Space: 42.1 MB (9.73%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,23 @@
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() % 2 == 1) {
-            return false;
-        }
+        if(s.length()%2==1) return false;
 
         Stack<Character> st = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(' || c == '{' || c == '[') {
+        for(char c: s.toCharArray()){
+            if( ( c==')' || c== ']' || c== '}' ) && st.isEmpty()){
+                    return false;
+            }
+            else if (c== '(' || c=='{' || c== '['){
                 st.push(c);
-            } else {
-                if (st.isEmpty()) {
-                    return false;
-                }
-                char top = st.pop();
-                if (!((top == '(' && c == ')') ||
-                        (top == '{' && c == '}') ||
-                        (top == '[' && c == ']'))) {
-                    return false;
-                }
+
+            }
+            else if ( c==')' || c== ']' || c== '}' ) {
+                char z = st.pop();
+                
+                if(z=='(' && c!=')') return false;
+                else if (z=='{' && c!='}') return false;
+                else if (z=='[' && c!=']') return false;
+
             }
         }
 
