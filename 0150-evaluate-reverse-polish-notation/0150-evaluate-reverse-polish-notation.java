9f6a35569494ea0ff47fc7e0ef069b7c3669e488
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-12-03 17:43:22 +0530

    Time: 5 ms (97.31%), Space: 44.6 MB (57.96%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,55 +1,27 @@
-// class Solution {
-//     public int evalRPN(String[] tokens) {
-//         Stack<String> st = new Stack<>();
-
-//         for(String i : tokens){
-//         if(!i.equals("+")&& !i.equals("-")&& !i.equals("*")&& !i.equals("/")){
-//             st.push(i);
-//         }else{
-//             int ans =0;
-//             int val1=Integer.parseInt(st.pop());
-//             int val2 = Integer.parseInt(st.pop());
-//             if(i.equals("+")) ans = val1+val2;
-//             else if(i.equals("-")) ans = val2-val1;
-//             else if(i.equals("*")) ans = val2*val1;
-//             else if(i.equals("/")) ans = val2/val1;
-
-//             String temp = Integer.toString(ans);
-//             st.push(temp);
-//         }
-//         }
-
-//         return Integer.parseInt(st.peek());
-//     }
-// }
-
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> st = new Stack<>();
 
-        for (String i : tokens) {
+        for(String i  :tokens){
+            switch(i){
+                case "+": st.push(st.pop()+st.pop());
+                break;
 
-            switch (i) {
-                case "+":
-                    stack.push(stack.pop() + stack.pop());
-                    break;
-                case "-":
-                    stack.push(-stack.pop() + stack.pop());
-                    break;
+                case "-": st.push(-st.pop()+st.pop());
+                break;
 
-                case "*":
-                    stack.push(stack.pop() * stack.pop());
-                    break;
+                case "*": st.push(st.pop()*st.pop());
+                break;
 
-                case "/":
-                    int n1 = stack.pop(), n2 = stack.pop();
-                    stack.push(n2 / n1);
-                    break;
-                default: stack.push(Integer.parseInt(i));
+                case "/": 
+                int a=st.pop();
+                int b=st.pop();
+                st.push(b/a);
+                break;
+                default: st.push(Integer.parseInt(i));
             }
         }
 
-        return stack.peek();
-
+        return st.peek();
     }
 }
