ec5bd0682b1e6387f09fbbb5125749e3fac37c19
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-12-13 08:53:25 +0530

    Time: 3 ms (99.51%), Space: 59.9 MB (50.93%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
@@ -1,32 +1,15 @@
 class Solution {
     public long findScore(int[] nums) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> 
-            a[0] == b[0] ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0])
-        );
-
-        for (int i = 0; i < nums.length; i++) {
-            pq.offer(new int[]{nums[i], i});
-        }
-
-        boolean[] marked = new boolean[nums.length];
-        long score = 0;
-
-        while (!pq.isEmpty()) {
-            int[] current = pq.poll();
-            int value = current[0];
-            int index = current[1];
-
-            if (marked[index]) {
-                continue;
+        long res = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            int start = i;
+            while (i + 1 < nums.length && nums[i + 1] < nums[i]) {
+                i++;
             }
-
-            score += value;
-
-            marked[index] = true;
-            if (index > 0) marked[index - 1] = true;
-            if (index < nums.length - 1) marked[index + 1] = true;
+            for (int j = i; j >= start; j -= 2) {
+                res += nums[j];
+            }
         }
-
-        return score;
+        return res;
     }
 }
