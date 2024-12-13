8ce7f6940273e91f68bf74cb4b216b7d2cc7f41f
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-12-13 20:10:09 +0530

    Time: 167 ms (55.88%), Space: 56.7 MB (96.76%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
@@ -1,15 +1,46 @@
+class Pair {
+    int val;
+    int ind;
+
+    Pair(int val, int ind) {
+        this.val = val;
+        this.ind = ind;
+    }
+}
+
 class Solution {
     public long findScore(int[] nums) {
-        long res = 0;
-        for (int i = 0; i < nums.length; i += 2) {
-            int start = i;
-            while (i + 1 < nums.length && nums[i + 1] < nums[i]) {
-                i++;
-            }
-            for (int j = i; j >= start; j -= 2) {
-                res += nums[j];
+        Queue<Pair> pq = new PriorityQueue<>((Pair a, Pair b) -> 
+            a.val == b.val ? a.ind - b.ind : a.val - b.val
+        );
+
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            pq.add(new Pair(nums[i], i));
+        }
+
+        boolean[] marked = new boolean[n];
+        long result = 0;
+
+        while (!pq.isEmpty()) {
+            Pair current = pq.poll();
+            int val = current.val;
+            int ind = current.ind;
+
+            if (!marked[ind]) {
+                result += val;
+                marked[ind] = true;
+
+                if (ind > 0) {
+                    marked[ind - 1] = true;
+                }
+                if (ind < n - 1) {
+                    marked[ind + 1] = true;
+                }
             }
         }
-        return res;
+
+        return result;
     }
 }
