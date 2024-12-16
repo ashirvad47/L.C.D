3347cd93a4db3f093ddfae9f2b79b801645a7b49
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2024-12-16 23:54:35 +0530

    Time: 1 ms (100%), Space: 45 MB (34.76%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
@@ -1,32 +1,16 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        PriorityQueue<Pair> queue = new PriorityQueue<>((a, b) -> a.val != b.val ? a.val - b.val : a.idx - b.idx);
-
-        for (int i = 0; i < nums.length; i++) {
-            queue.offer(new Pair(nums[i], i));
+        for(int i=0;i<k;i++){
+            int index=findMinI(nums);
+            nums[index]*=multiplier;
         }
-
-        while (k-- > 0) {
-            Pair smallest = queue.poll();
-            smallest.val *= multiplier;
-            queue.offer(smallest);
-        }
-
-        while (!queue.isEmpty()) {
-            Pair pair = queue.poll();
-            nums[pair.idx] = pair.val;
-        }
-
         return nums;
     }
-}
-
-class Pair {
-    int val;
-    int idx;
-
-    Pair(int val, int idx) {
-        this.val = val;
-        this.idx = idx;
+    public int findMinI(int[] arr ){
+        int min =0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[min]>arr[i])min=i;
+        }
+        return min;
     }
 }
