eb4ebcbcf41c11ccc0a19b1be464fa335e7d3019
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2025-01-17 20:34:02 +0530

    Time: 2 ms (100%), Space: 63 MB (14.71%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean doesValidArrayExist(int[] derived) {
         int res=0;
         for (int i : derived) res^=i;
-        // res ^= derived[0];
 
         return res==0;
     }
