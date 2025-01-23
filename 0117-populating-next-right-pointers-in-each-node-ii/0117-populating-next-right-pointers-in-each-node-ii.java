80ceb78f59c62da154292c521390ccdd5cd87ea8
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2025-01-23 19:16:28 +0530

    Time: 0 ms (100%), Space: 44.4 MB (38.04%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -21,32 +21,62 @@ class Node {
 };
 */
 
+// class Solution {
+//     public Node connect(Node root) {
+//         if (root == null) return null; 
+
+//         Queue<Node> qu = new LinkedList<>();
+//         qu.offer(root);
+
+//         while (!qu.isEmpty()) {
+//             int levelSize = qu.size();
+//             while (levelSize != 0) {
+//                 levelSize--;
+//                 Node node = qu.poll();
+
+//                 if (levelSize > 0) {
+//                     node.next = qu.peek();
+//                 }
+//                 if (node.left != null) {
+//                     qu.offer(node.left);
+//                 }
+//                 if (node.right != null) {
+//                     qu.offer(node.right);
+//                 }
+//             }
+
+//         }
+
+//         return root;
+//     }
+// }
+
+//faster
 class Solution {
     public Node connect(Node root) {
-        if (root == null) return null; 
-
-        Queue<Node> qu = new LinkedList<>();
-        qu.offer(root);
-
-        while (!qu.isEmpty()) {
-            int levelSize = qu.size();
-            while (levelSize != 0) {
-                levelSize--;
-                Node node = qu.poll();
-
-                if (levelSize > 0) {
-                    node.next = qu.peek();
-                }
-                if (node.left != null) {
-                    qu.offer(node.left);
-                }
-                if (node.right != null) {
-                    qu.offer(node.right);
-                }
-            }
-
+        if(root==null){
+            return null;
         }
 
+        Node head = root;
+        while(head!=null){
+            Node dummy = new Node(0);
+            Node temp=dummy; 
+
+            while(head!=null){
+                if(head.left !=null){
+                    temp.next=head.left;
+                    temp=temp.next;
+                }
+                if(head.right !=null){
+                    temp.next =head.right;
+                    temp=temp.next;
+                }
+                head = head.next;
+            }
+            head = dummy.next;
+
+        }
         return root;
     }
 }
