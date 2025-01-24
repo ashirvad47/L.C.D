b67e594dd65f40b4651ddeb50a78e00357568ed5
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2025-01-24 13:51:02 +0530

    Time: 4 ms (99.02%), Space: 55.7 MB (24.54%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -1,26 +1,32 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<Integer> res = new ArrayList<>();
-        if(graph == null || graph.length == 0)  return res;
-        
+        if (graph == null || graph.length == 0)
+            return res;
+
         int nodeCount = graph.length;
         int[] color = new int[nodeCount];
-        
-        for(int i = 0;i < nodeCount;i++){
-            if(dfs(graph, i, color))    res.add(i);
+
+        for (int i = 0; i < nodeCount; i++) {
+            if (dfs(graph, i, color))
+                res.add(i);
         }
-        
+
         return res;
     }
-    public boolean dfs(int[][] graph, int start, int[] color){
-        if(color[start] != 0)   return color[start] == 1;
-        
+
+    public boolean dfs(int[][] graph, int start, int[] color) {
+        if (color[start] == 1)
+            return true;
+        if (color[start] == 2)
+            return false;
         color[start] = 2;
-        for(int newNode : graph[start]){
-            if(!dfs(graph, newNode, color))    return false;
+        for (int newNode : graph[start]) {
+            if (dfs(graph, newNode, color) == false)
+                return false;
         }
         color[start] = 1;
-        
+
         return true;
     }
 }
