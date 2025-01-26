57541b3da6d8e9eb35b843b9d679f17fea05c63f
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2025-01-26 22:38:42 +0530

    Time: 20 ms (88.16%), Space: 60.9 MB (61.18%) - LeetHub

--- 2246-maximum-employees-to-be-invited-to-a-meeting/2246-maximum-employees-to-be-invited-to-a-meeting.java
+++ 2246-maximum-employees-to-be-invited-to-a-meeting/2246-maximum-employees-to-be-invited-to-a-meeting.java
@@ -1,68 +1,47 @@
 class Solution {
-    public int maximumInvitations(int[] favorite) {
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < favorite.length; i++) {
-            graph.add(new ArrayList<>());
+        public int maximumInvitations(int[] favorite) {
+        int n = favorite.length;
+        boolean[] visited = new boolean[n];
+        // topological sort which picks out acyclic part.
+        int[] indegrees = new int[n];
+        for(int i = 0; i < n; ++i) {
+            int j = favorite[i]; // i -> favorite[i].
+            ++indegrees[j];
         }
-
-        int answer = 0;
-
-        List<List<Integer>> pairs = new ArrayList<>();
-        for (int i = 0; i < favorite.length; i++) {
-            if (i == favorite[favorite[i]]) {
-                if (i < favorite[i]) {
-                    List<Integer> pair = new ArrayList<>();
-                    pair.add(i);
-                    pair.add(favorite[i]);
-                    pairs.add(pair);
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < n; ++i) {
+            if (indegrees[i] == 0) {
+                visited[i] = true;
+                q.offer(i);
+            }
+        }
+        int[] dp = new int[n]; // dp[i] is the longest path leading to i exclusively.
+        while(!q.isEmpty()) {
+            int i = q.poll();
+            int j = favorite[i];
+            dp[j] = Math.max(dp[j], dp[i] + 1);
+            if (--indegrees[j] == 0) {
+                visited[j] = true;
+                q.offer(j);
+            }
+        }
+        // now not visited nodes are all loops. check each loop's length.
+        int result = 0; // loops of length > 2.
+        int result2 = 0; // loops of length 2 and paths leading to both nodes.
+        for(int i = 0; i < n; ++i) {
+            if (visited[i] == false) {
+                int length = 0;
+                for(int j = i; visited[j] == false; j = favorite[j]) {
+                    visited[j] = true;
+                    ++length;
                 }
-            } else {
-                graph.get(favorite[i]).add(i);
+                if (length == 2) {
+                    result2 += 2 + dp[i] + dp[favorite[i]];
+                } else {
+                    result = Math.max(result, length);
+                }
             }
         }
-
-        boolean[] visited = new boolean[favorite.length];
-        for (List<Integer> pair: pairs) {
-            answer += dfs(graph, pair.get(0), visited) + dfs(graph, pair.get(1), visited);
-        }
-        
-        int[] counter = new int[favorite.length];
-        int[] round = new int[favorite.length];
-
-        int rnd = 1;
-
-        int circleMax = 0;
-
-        for (int i = 0; i < favorite.length; i++) {
-            if (visited[i]) {
-                continue;
-            }
-            if (round[i] != 0) {
-                continue;
-            }
-
-            int cnt = 1;
-            int j = i;
-            while (counter[j] == 0) {
-                counter[j] = cnt;
-                round[j] = rnd;
-                j = favorite[j];
-                cnt++;
-            }
-            if (round[j] == rnd) {
-                circleMax = Math.max(circleMax, cnt - counter[j]);
-            }
-            rnd++;
-        }
-        return Math.max(circleMax, answer);
-    }
-
-    private int dfs(List<List<Integer>> graph, int node, boolean[] visited) {
-        visited[node] = true;
-        int max = 0;
-        for (int neighbor: graph.get(node)) {
-            max = Math.max(max, dfs(graph, neighbor, visited));
-        }
-        return max + 1;
+        return Math.max(result, result2);
     }
 }
