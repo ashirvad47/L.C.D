2babbe5f4aa7aa94d6e776e3460d9bdf91c7dc5e
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2025-02-08 10:50:38 +0530

    Time: 83 ms (54.11%), Space: 98.6 MB (62.33%) - LeetHub

---
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.java
@@ -0,0 +1,31 @@
+class NumberContainers {
+    private HashMap<Integer, Integer> indexToNum;
+    private HashMap<Integer, TreeSet<Integer>> numToIndices;
+    
+    public NumberContainers() {
+        indexToNum = new HashMap<>();
+        numToIndices = new HashMap<>();
+    }
+    
+    public void change(int index, int number) {
+        if (indexToNum.containsKey(index)) {
+            int oldNum = indexToNum.get(index);
+            numToIndices.get(oldNum).remove(index);
+            if (numToIndices.get(oldNum).isEmpty()) {
+                numToIndices.remove(oldNum);
+            }
+        }
+        
+        indexToNum.put(index, number);
+        
+        numToIndices.putIfAbsent(number, new TreeSet<>());
+        numToIndices.get(number).add(index);
+    }
+    
+    public int find(int number) {
+        if (numToIndices.containsKey(number)) {
+            return numToIndices.get(number).first();
+        }
+        return -1;
+    }
+}
