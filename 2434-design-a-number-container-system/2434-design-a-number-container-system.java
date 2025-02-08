f8d89db9ff90d99c45aeabacdda11d28267b0f72
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2025-02-08 12:10:07 +0530

    Time: 90 ms (21.23%), Space: 95.8 MB (96.58%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.java
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.java
@@ -1,34 +1,32 @@
 class NumberContainers {
-
-    HashMap<Integer,Integer> hmap = new HashMap<>();
-    TreeMap<Integer,TreeSet<Integer>> tmap = new TreeMap<>();
+    HashMap<Integer, Integer> hmap = new HashMap<>();
+    HashMap<Integer, TreeSet<Integer>> tmap = new HashMap<>();
 
     public NumberContainers() {
-        
     }
-    
+
     public void change(int index, int number) {
-        if(hmap.containsKey(index)){
-            
-            if(tmap.containsKey(hmap.get(index))){
-                tmap.get(hmap.get(index)).remove(index);
-                if(tmap.get(hmap.get(index)).isEmpty()) tmap.remove(hmap.get(index));
+        if (hmap.containsKey(index)) {
+            int oldNumber = hmap.get(index);
+            TreeSet<Integer> oldSet = tmap.get(oldNumber);
+            if (oldSet != null) {
+                oldSet.remove(index);
+                if (oldSet.isEmpty()) {
+                    tmap.remove(oldNumber);
+                }
             }
         }
-        hmap.put(index,number);
-        tmap.putIfAbsent(number, new TreeSet<>());
-        tmap.get(number).add(index);
 
+        hmap.put(index, number);
+        tmap.computeIfAbsent(number, k -> new TreeSet<>()).add(index);
     }
-    
-    public int find(int number) {
-        if(!tmap.containsKey(number) || tmap.get(number).isEmpty()){
-            return -1;
-        }
 
-        return tmap.get(number).first();
+    public int find(int number) {
+        TreeSet<Integer> indices = tmap.get(number);
+        return (indices == null || indices.isEmpty()) ? -1 : indices.first();
     }
 }
+
 
 /**
  * Your NumberContainers object will be instantiated and called as such:
