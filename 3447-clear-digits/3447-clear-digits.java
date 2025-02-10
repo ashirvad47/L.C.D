43db50527310326956ceb4ba2835708830f02f32
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2025-02-10 09:55:04 +0530

    Time: 1 ms (100%), Space: 42.4 MB (85.44%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.java
+++ 3447-clear-digits/3447-clear-digits.java
@@ -1,15 +1,31 @@
+// class Solution {
+//     public String clearDigits(String s) {
+//         StringBuilder sb = new StringBuilder();
+//         Deque<Character> st = new ArrayDeque<>();
+
+//         for(char c :s.toCharArray()){
+//             if(c>='0' && c<='9') st.pop();
+//             else st.push(c);
+//         }
+
+//         for(char c : st) sb.append(c);
+
+//         return sb.reverse().toString();
+//     }
+// }
+
 class Solution {
     public String clearDigits(String s) {
         StringBuilder sb = new StringBuilder();
-        Deque<Character> st = new ArrayDeque<>();
 
-        for(char c :s.toCharArray()){
-            if(c>='0' && c<='9') st.pop();
-            else st.push(c);
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                if (sb.length() > 0) sb.setLength(sb.length() - 1);
+            } else {
+                sb.append(c);
+            }
         }
 
-        for(char c : st) sb.append(c);
-
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
