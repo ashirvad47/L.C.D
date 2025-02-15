129a4014dd9a78513fb109663c4a3d968e8dd165
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2025-02-15 11:36:22 +0530

    Time: 33 ms (66.77%), Space: 44.2 MB (66.46%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
@@ -1,16 +1,32 @@
 class Solution {
-    int arr[] = { 1, 9, 10, 36, 45, 55, 82, 91, 99, 100, 235, 297, 369, 370, 379, 414, 657, 675, 703, 756, 792, 909,
-            918, 945, 964, 990, 991, 999, 1000 };
-    
-    public int punishmentNumber(int n) {
-        int res = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] <= n) {
-                res += arr[i] * arr[i];
-            } else {
-                break;
+    private boolean isValidPartition(int num, int square, int sum, int index) {
+        if (index == String.valueOf(square).length()) {
+            return sum == num;
+        }
+        
+        int currentSum = 0;
+        String squareStr = String.valueOf(square);
+        
+        for (int i = index; i < squareStr.length(); i++) {
+            currentSum = currentSum * 10 + (squareStr.charAt(i) - '0');
+            if (isValidPartition(num, square, sum + currentSum, i + 1)) {
+                return true;
             }
         }
-        return res;
+        return false;
+    }
+
+    public int punishmentNumber(int n) {
+        int punishmentSum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 9 != 0 && i % 9 != 1) continue;
+
+            int square = i * i;
+            if (isValidPartition(i, square, 0, 0)) {
+                punishmentSum += square;
+            }
+        }
+        return punishmentSum;
     }
 }
