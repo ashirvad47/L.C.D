956539c92bb162601fea8491f4f4de4a866895ac
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2025-04-07 00:58:54 +0530

    Time: 0 ms (100%), Space: 41.5 MB (89.2%) - LeetHub

--- 1976-splitting-a-string-into-descending-consecutive-values/1976-splitting-a-string-into-descending-consecutive-values.java
+++ 1976-splitting-a-string-into-descending-consecutive-values/1976-splitting-a-string-into-descending-consecutive-values.java
@@ -1,22 +1,20 @@
 class Solution {
     public boolean splitString(String s) {
-        return backTrack( s , 0, -1, 0 );
+        return helper(s,0,-1,0);
     }
-    
-    public boolean backTrack( String s, int index, long prevNumber, int count )
-    {
-        if( index >= s.length() ){
-            if( count >= 2) return true;
+
+    private boolean helper(String s, int idx, long prev, int count){
+        if(idx >= s.length()) {
+            if(count>=2)return true;
             else return false;
         }
-        
-        long currNo=0;
-        for( int i = index; i < s.length() ;i++ )
-        {
-            currNo = currNo*10+(s.charAt(i)-'0');
-            if( prevNumber == -1 || ( prevNumber - currNo ) == 1 )
-                if( backTrack( s,i+1,currNo, count+1) ) return true;
+        long curr =0;
+        for(int i =idx;i < s.length();i++){
+            curr = curr*10 + (s.charAt(i)-'0');
+            if(prev==-1 || (prev-curr)==1) if (helper(s,i+1,curr,count+1))return true;
         }
-    return false;
+
+        return  false;
+
     }
 }
