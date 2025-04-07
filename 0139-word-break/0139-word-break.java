fb6447bc0dafd8359a0bf41da9ca43c9802f89a8
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2025-04-08 01:29:47 +0530

    Time: 6 ms (60.97%), Space: 45 MB (15.87%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Boolean[] memo = new Boolean[s.length()];
+        return helper(s,wordDict,0,memo);
+    }
+
+    private boolean helper(String s, List<String> wordDict, int idx, Boolean memo[]) {
+        if (idx == s.length())
+            return true;
+        if (memo[idx] != null)
+            return memo[idx];
+
+        boolean found = false;
+        for(int i =idx+1;i <= s.length();i++){
+            String pref = s.substring(idx,i);
+
+            if(wordDict.contains(pref)){
+                if(helper(s,wordDict,i,memo)) {
+                    found=true;
+                    break;
+                }
+            }
+        }
+
+        memo[idx] =found;
+        return found;
+
+    }
+}
