6a72b0021a36e007af80e0d5077f222bc6edd450
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2025-04-08 11:09:48 +0530

    Time: 6 ms (60.97%), Space: 44.5 MB (46.68%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,29 +1,27 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Boolean[] memo = new Boolean[s.length()];
-        return helper(s,wordDict,0,memo);
+        Boolean []memo = new Boolean[s.length()];
+
+        return wordBreak(s,wordDict,0,memo);
     }
 
-    private boolean helper(String s, List<String> wordDict, int idx, Boolean memo[]) {
-        if (idx == s.length())
-            return true;
-        if (memo[idx] != null)
-            return memo[idx];
+    private boolean wordBreak(String s, List<String> wordDict, int idx, Boolean [] memo){
+            if(idx==s.length()) return true;
 
-        boolean found = false;
-        for(int i =idx;i <= s.length();i++){
-            String pref = s.substring(idx,i);
+            if(memo[idx]!=null) return memo[idx];
 
-            if(wordDict.contains(pref)){
-                if(helper(s,wordDict,i,memo)) {
-                    found=true;
-                    break;
+            boolean found;
+            for(int i = idx+1; i<= s.length();i++){
+                String sub = s.substring(idx,i);
+
+                if(wordDict.contains(sub)) if(wordBreak(s,wordDict,i , memo)) {
+                    found = true;
+                    return found;
                 }
             }
-        }
 
-        memo[idx] =found;
-        return found;
 
+            
+            return memo[idx] = false;
     }
 }
