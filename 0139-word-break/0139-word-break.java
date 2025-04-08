6606ed72ca9d942b669f72b9a8b9d79835edc65b
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2025-04-08 11:40:06 +0530

    Time: 3 ms (79.67%), Space: 43.3 MB (61.82%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,27 +1,49 @@
-class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
-        Boolean []memo = new Boolean[s.length()];
+//TOP TO BOTTOM (MEMOIZATION)
 
-        return wordBreak(s,wordDict,0,memo);
-    }
+// class Solution {
+//     public boolean wordBreak(String s, List<String> wordDict) {
+//         Boolean []memo = new Boolean[s.length()];
 
-    private boolean wordBreak(String s, List<String> wordDict, int idx, Boolean [] memo){
-            if(idx==s.length()) return true;
+//         return wordBreak(s,wordDict,0,memo);
+//     }
 
-            if(memo[idx]!=null) return memo[idx];
+//     private boolean wordBreak(String s, List<String> wordDict, int idx, Boolean [] memo){
+//             if(idx==s.length()) return true;
 
-            boolean found;
-            for(int i = idx+1; i<= s.length();i++){
-                String sub = s.substring(idx,i);
+//             if(memo[idx]!=null) return memo[idx];
 
-                if(wordDict.contains(sub)) if(wordBreak(s,wordDict,i , memo)) {
-                    found = true;
-                    return found;
-                }
-            }
+//             boolean found;
+//             for(int i = idx+1; i<= s.length();i++){
+//                 String sub = s.substring(idx,i);
+
+//                 if(wordDict.contains(sub)) if(wordBreak(s,wordDict,i , memo)) {
+//                     found = true;
+//                     return found;
+//                 }
+//             }
 
 
             
-            return memo[idx] = false;
+//             return memo[idx] = false;
+//     }
+// }
+
+//BOTTOM TO TOP (TABULATION)
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] tab = new boolean[s.length() + 1];
+        tab[0] = true;
+
+        for(int i = 1; i <= s.length(); i++) {
+            for(int j = i - 1; j >= 0; j--) {
+                if(tab[j] && wordDict.contains(s.substring(j, i))) {
+                    tab[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return tab[s.length()];
     }
 }
