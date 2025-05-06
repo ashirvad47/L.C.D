69666087fba17784ea409db625ae811672d60314
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2025-05-06 13:38:20 +0530

    Time: 1 ms (98.72%), Space: 45.4 MB (44.27%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -1,24 +1,24 @@
-// class Solution {
-//     public int[] buildArray(int[] nums) {
-//         int []ans = new int[nums.length];
-//         for(int i =0;i<nums.length;i++){
-//             ans[i] = nums[nums[i]];
-//         }
-//         return ans;
-//     }
-// }
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int []ans = new int[nums.length];
+        for(int i =0;i<nums.length;i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
 
 //O(1) space below
 
-class Solution {
-    public int[] buildArray(int[] nums) {
-        int n =nums.length;
-        for(int i =0;i<n;i++){
-           nums[i]=nums[i] + n*(nums[nums[i]]%n);
-        }
-        for(int i =0; i <n;i++){
-            nums[i]=nums[i]/n;
-        }
-        return nums;
-    }
-}
+// class Solution {
+//     public int[] buildArray(int[] nums) {
+//         int n =nums.length;
+//         for(int i =0;i<n;i++){
+//            nums[i]=nums[i] + n*(nums[nums[i]]%n);
+//         }
+//         for(int i =0; i <n;i++){
+//             nums[i]=nums[i]/n;
+//         }
+//         return nums;
+//     }
+// }
