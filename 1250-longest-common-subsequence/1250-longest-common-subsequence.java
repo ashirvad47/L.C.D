6e0a460795a64224d40d644a21f36458a00c9335
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2025-05-07 14:49:04 +0530

    Time: 19 ms (87.64%), Space: 51 MB (42.21%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,21 +1,43 @@
+// TOP DOWN MEMOIZATION
+
+// class Solution {
+//     public int longestCommonSubsequence(String text1, String text2) {
+//         int n= text1.length();
+//         int m= text2.length();
+
+//         int dp[][]= new int [n+1][m+1];
+//         for(int i[] : dp) Arrays.fill(i,-1);
+
+//         return longestCommonSubsequence(text1,text2,n,m,dp);
+//     }
+
+//     public int longestCommonSubsequence(String text1, String text2,int n,int m, int dp[][]){
+//         if(n==0 || m==0) return 0;
+
+//         if(dp[n][m]!=-1) return dp[n][m];
+
+//         if(text1.charAt(n-1)==text2.charAt(m-1)) return dp[n][m]= 1+ longestCommonSubsequence(text1,text2,n-1,m-1,dp);
+
+//         else return dp[n][m] =Math.max(longestCommonSubsequence(text1,text2,n,m-1,dp),longestCommonSubsequence(text1,text2,n-1,m,dp));
+//     }
+// }
+
+// BOTTOM UP TABULATION
+
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n= text1.length();
         int m= text2.length();
 
         int dp[][]= new int [n+1][m+1];
-        for(int i[] : dp) Arrays.fill(i,-1);
 
-        return longestCommonSubsequence(text1,text2,n,m,dp);
-    }
+        for(int i =1; i< dp.length;i++)
+            for(int j =1;j < dp[0].length;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)) dp[i][j] = 1+dp[i-1][j-1]; 
+                else dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
 
-    public int longestCommonSubsequence(String text1, String text2,int n,int m, int dp[][]){
-        if(n==0 || m==0) return 0;
+        return dp[n][m];
 
-        if(dp[n][m]!=-1) return dp[n][m];
-
-        if(text1.charAt(n-1)==text2.charAt(m-1)) return dp[n][m]= 1+ longestCommonSubsequence(text1,text2,n-1,m-1,dp);
-
-        else return dp[n][m] =Math.max(longestCommonSubsequence(text1,text2,n,m-1,dp),longestCommonSubsequence(text1,text2,n-1,m,dp));
     }
 }
