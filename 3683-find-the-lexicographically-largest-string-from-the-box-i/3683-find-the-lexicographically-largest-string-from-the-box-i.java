c54a99c1d04861c5732cbaf286ff0fb70351a2f7
Ashirvad Samanta <142304893+ashirvad47@users.noreply.github.com>
2025-06-04 17:45:06 +0530

    Time: 5 ms (79.63%), Space: 45.4 MB (50.93%) - LeetHub

---
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String answerString(String word, int numFriends) {
+    if(numFriends == 1) return word;
+    int n = word.length();
+    int maxlen = (n-numFriends+1);
+    char maxchar = word.charAt(0);
+    String res = "";
+    for(int i=0; i<n; i++) {
+        if(word.charAt(i) >= maxchar) {
+            String curr = word.substring(i, Math.min(i+maxlen, n));
+            if(curr.compareTo(res) > 0) res = curr;
+            maxchar = word.charAt(i);
+        }
+    }
+    return res;
+}
+}
